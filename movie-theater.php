a0dfcea68b10df1032a9a52ed75fe677facabb12
Ryan <ryan.graebert@gmail.com>
2016-01-29 08:00:52 -0500

    changed to v1.6.1    - Added a NON-FUNCTIONING option menu with sample text.

--- movie-theater.php
+++ movie-theater.php
@@ -4,7 +4,7 @@
 Plugin Name: Movie Theater
 Plugin URI: http://lightmarkcreative.com/movietheater
 Description: Custom post type “films” and “showtimes” (with ticket links) which can then be displayed as a sortable list on a page, and also individually as posts.  Content can be automatically generated and updated from ticket server xml/json feed.
-Version: 1.6.0
+Version: 1.6.1
 Author: Chris, Ryan
 Author URI: http://lightmarkcreative.com
 License: GPL2
@@ -18,7 +18,7 @@ include('classes/movietheater_Film.php');
 
 require_once ('inc/custom-post-types.php');
 include('inc/load-ticket-servers.php');
-
+//include('inc/movieTheater_settingsPanel.php');
 
 
 //=========================== Actions & Filters ==============//
@@ -44,6 +44,95 @@ function my_deactivation() {
 
 
 //=========================== Functions ======================//
+
+// ------------------------------------------------------------------
+// Add all your sections, fields and settings during admin_init
+// ------------------------------------------------------------------
+//
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+add_action( 'admin_init', 'eg_settings_api_init' );
+
+function my_plugin_menu() {
+
+    //add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);
+    add_options_page(
+        'Movie Theater Control Panel',
+        'Movie Theater CP',
+        'manage_options',
+        'movie',
+        'movie_theater_admin_options'
+    );
+}
+function movie_theater_admin_options()
+{
+
+    do_settings_sections( 'movie_theater_settings' );
+}
+
+function eg_settings_api_init() {
+    // Add the section to reading settings so we can add our
+    // fields to it
+    add_settings_section(
+        'movie_theater_advanced_options',
+        'Movie Theater Advanced Settings',
+        'eg_setting_section_callback_function',
+        'movie_theater_settings'
+    );
+
+    // Add the field with the names and function to use for our new
+    // settings, put it in our new section
+    add_settings_field(
+        'eg_movie_theater_checkbox',
+        'Test Checkbox',
+        'eg_movie_theater_checkbox_setting_callback_function',
+        'movie_theater_settings',
+        'movie_theater_advanced_options'
+    );
+    add_settings_field(
+        'eg_movie_theater_text',
+        'Test text field',
+        'eg_movie_theater_text_setting_callback_function',
+        'movie_theater_settings',
+        'movie_theater_advanced_options'
+    );
+
+    // Register our setting so that $_POST handling is done for us and
+    // our callback function just has to echo the <input>
+    register_setting( 'movie_theater_settings', 'eg_setting_name' );
+} // eg_settings_api_init()
+
+
+
+// ------------------------------------------------------------------
+// Settings section callback function
+// ------------------------------------------------------------------
+//
+// This function is needed if we added a new section. This function
+// will be run at the start of our section
+//
+
+function eg_setting_section_callback_function() {
+    echo '<p>Below are some amazing options I know you are going to want to mess with</p>';
+}
+
+// ------------------------------------------------------------------
+// Callback function for our example setting
+// ------------------------------------------------------------------
+//
+// creates a checkbox true/false option. Other types are surely possible
+//
+
+function eg_movie_theater_checkbox_setting_callback_function() {
+    echo '<input name="eg_movie_theater_checkbox" id="eg_movie_theater_checkbox" type="checkbox" value="1" class="code" ' . checked( 1, get_option( 'eg_movie_theater_checkbox' ), false ) . ' /> Explanation text';
+}
+function eg_movie_theater_text_setting_callback_function() {
+    echo '<input name="eg_movie_theater_text" id="eg_movie_theater_text" type="text" value="fill me out" /> Explanation text';
+    //since this is our last field time for the submit button!
+    echo '<br /> <br />';
+    echo '<input type="submit" value="Save Changes" />';
+}
+
 
 //main function that runs everything
 
